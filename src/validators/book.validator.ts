63387e5f47ca5d3f8338ceaf5ec6c010c21927a2
Gerald Andreiman <geraldandreiman5b@gmail.com>
2026-03-25 11:15:04 +0200

    Added book and review validator

--- src/validators/book.validator.ts
+++ src/validators/book.validator.ts
@@ -1,1 +1,12 @@
 import { z } from "zod";
+
+export const bookSchema = z.object({
+  title: z.string().min(1),
+  authorId: z.string(),
+  publisherId: z.string(),
+  genre: z.string(),
+  language: z.string(),
+  publishedYear: z.number().int().gte(1500),
+});
+
+export type BookInput = z.infer<typeof bookSchema>;
