002f15a719d04405f6161383e8586d4440433184
Gerald Andreiman <geraldandreiman5b@gmail.com>
2026-03-25 11:08:29 +0200

    Added data and zod validator

---
+++ src/data/book.ts
@@ -0,0 +1,22 @@
+import { Book } from "../models/book";
+export const books: Book[] = [
+  {
+    id: "1",
+    title: "Clean Code",
+    authorId: "a1",
+    publisherId: "p1",
+    genre: "Programming",
+    language: "EN",
+    publishedYear: 2008,
+  },
+  {
+    id: "2",
+    title: "The Pragmatic Programmer",
+    authorId: "a2",
+    publisherId: "p2",
+    genre: "Programming",
+    language: "EN",
+    publishedYear: 1999,
+  },
+  // add up to 10–15
+];
